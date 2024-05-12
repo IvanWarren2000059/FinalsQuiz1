045e70f719a63990e8d23f35212c47c83e2ea4f6
Yam yam <138616148+notyamyam@users.noreply.github.com>
2024-05-12 23:30:34 +0800

    unfinish register routing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,31 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import UserLogin from '@/views/UserLogin.vue';
-import HomeFile from '@/views/HomeFile.vue';
-import EditPost from '../views/EditPost.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import UserLogin from "@/views/UserLogin.vue";
+import HomeFile from "@/views/HomeFile.vue";
+import EditPost from "../views/EditPost.vue";
+import RegisterPage from "../views/RegisterPage.vue";
 
 const routes = [
-  { path: '/', component: UserLogin },
-  { path: '/home', component: HomeFile, meta: { requiresAuth: true } },
-  { 
-    path: '/edit/:id', 
-    component: EditPost, 
-    name: 'EditPost', 
-    props: true, 
-    meta: { requiresAuth: true } 
+  { path: "/", component: UserLogin },
+  { path: "/home", component: HomeFile, meta: { requiresAuth: true } },
+  {
+    path: "/edit/:id",
+    component: EditPost,
+    name: "EditPost",
+    props: true,
+    meta: { requiresAuth: true },
+  },
+
+  {
+    path: "/register",
+    component: RegisterPage,
+    name: "RegisterPage",
+    props: true,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (to.meta.requiresAuth && !token) {
-    next('/');
-  } else if (to.path !== '/' && !token) {
-    next('/');
+    next("/");
+  } else if (to.path !== "/" && !token) {
+    next("/");
   } else {
     next();
   }
