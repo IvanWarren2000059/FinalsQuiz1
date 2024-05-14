abaa9b68e59b57ab2ef28b51b7dda826f2c2708b
Yam yam <138616148+notyamyam@users.noreply.github.com>
2024-05-14 21:03:18 +0800

    redesign login

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -1,50 +1,113 @@
 <template>
-  <div class="login-page">
-    <h2>Login</h2>
-    <form @submit.prevent="login">
-      <input type="email" v-model="email" placeholder="Email" required>
-      <input type="password" v-model="password" placeholder="Password" required>
-      <button type="submit" class="login-btn">Login</button>
-      <button type="button" @click="redirectToRegister" class="register-btn">Register</button>
-    </form>
+  <div class="bg-gray-100">
+    <div
+      class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+    >
+      <div
+        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0"
+      >
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1
+            class="font-sans text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-2xl"
+          >
+            Sign in to your account
+          </h1>
+
+          <form
+            class="space-y-4 md:space-y-6"
+            action="#"
+            @submit.prevent="login"
+          >
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-"
+                >Your email</label
+              >
+              <input
+                v-model="email"
+                type="email"
+                name="email"
+                id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring focus:ring-blue-500 block w-full p-2.5"
+                placeholder="example@sample.com"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                for="password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-"
+                >Password</label
+              >
+              <input
+                v-model="password"
+                type="password"
+                name="password"
+                id="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring focus:ring-blue-500 block w-full p-2.5"
+                placeholder="••••••••"
+                required=""
+              />
+            </div>
+
+            <button
+              type="submit"
+              class="w-full text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-blue-500 font-medium rounded-lg px-5 py-2.5 text-center"
+            >
+              Sign in
+            </button>
+          </form>
+
+          <div class="flex flex-col items-center justify-between">
+            <button type="submit">
+              <Label
+                >Dont have an account?
+                <button class="text-blue-500">Register</button></Label
+              >
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'; // Import Axios directly
+import axios from "axios"; // Import Axios directly
 
 export default {
   name: "UserLogin",
   data() {
     return {
-      email: '',
-      password: ''
+      email: "",
+      password: "",
     };
   },
   methods: {
-login() {
-  axios.post('http://localhost:8000/api/login', { 
-    email: this.email,
-    password: this.password
-  })
-  .then(response => {
-    console.log("Response data:", response.data); // Log the entire response data
-    localStorage.setItem('token', response.data.access_token);
-      localStorage.setItem('userId', response.data.id);
-      localStorage.setItem('userType', response.data.user_Type);
- 
-    
-    this.$router.push('/home');
-    console.log("Success")
-  })
-  .catch(error => {
-    console.error('Login failed:', error);
-  });
-},
+    login() {
+      axios
+        .post("http://localhost:8000/api/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
+          console.log("Response data:", response.data); // Log the entire response data
+          localStorage.setItem("token", response.data.access_token);
+          localStorage.setItem("userId", response.data.id);
+          localStorage.setItem("userType", response.data.user_Type);
+
+          this.$router.push("/home");
+          console.log("Success");
+        })
+        .catch((error) => {
+          console.error("Login failed:", error);
+        });
+    },
     redirectToRegister() {
-      this.$router.push('/register');
-    }
-  }
+      this.$router.push("/register");
+    },
+  },
 };
 </script>
 
@@ -64,8 +127,9 @@ input {
   border-radius: 4px;
 }
 
-.login-btn, .register-btn {
-  background-color: #4CAF50;
+.login-btn,
+.register-btn {
+  background-color: #4caf50;
   color: white;
   border: none;
   border-radius: 4px;
@@ -73,7 +137,8 @@ input {
   padding: 10px 20px;
 }
 
-.login-btn:hover, .register-btn:hover {
+.login-btn:hover,
+.register-btn:hover {
   opacity: 0.8;
 }
 </style>
